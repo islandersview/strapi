f4ee6fea2d33e9ec88381af3b04443c83e9ec5c9
John Roice Aldeza <jroicealdeza@gmail.com>
2024-07-24 10:13:10 +0800

    chore: Add item-listing collection type

---
+++ src/api/item-listing/controllers/item-listing.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * item-listing controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::item-listing.item-listing');
---
+++ src/api/item-listing/routes/item-listing.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * item-listing router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::item-listing.item-listing');
